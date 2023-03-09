a23c27736de6387eba12735f1ba93b8e42008df0
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-09 11:21:33 -0500

    Part 2 Follow Along - Adding styles to React App

--- follow-along/src/App.js
+++ follow-along/src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Note from './components/Note'
 import { useState, useEffect } from 'react'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = ({ props }) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => setNotes(initialNotes))
@@ -22,7 +24,12 @@ const App = ({ props }) => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
       })
       .catch((error) => {
-        alert(`The note '${note.content}' was already deleted from the server`)
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter((n) => n.id !== id))
       })
   }
@@ -53,6 +60,7 @@ const App = ({ props }) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
--- follow-along/src/components/Note.js
+++ follow-along/src/components/Note.js
@@ -2,7 +2,7 @@ const Note = ({ note, toggleImportance }) => {
   const label = note.important ? 'make not important' : 'make important'
 
   return (
-    <li key={note.id}>
+    <li key={note.id} className="note">
       {note.content} <button onClick={toggleImportance}> {label}</button>
     </li>
   )
