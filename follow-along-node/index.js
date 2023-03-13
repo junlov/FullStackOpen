49b3243aa1453b9d2c662aa0ee260fa10ee23817
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-13 12:19:18 -0400

    Moved to new repository for deployment to Render

--- follow-along-node/index.js
+++ follow-along-node/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const requestLogger = (request, response, next) => {
@@ -89,5 +92,5 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
