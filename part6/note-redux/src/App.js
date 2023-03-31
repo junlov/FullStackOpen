3d4968b0fbf1206fd0513c4ae4b639307ad415c7
Junior Love <K4ngar0o@proton.me>
2023-03-31 10:40:04 -0500

    Part 6 Follow Along - Redux Toolkit Integration

--- part6/note-redux/src/App.js
+++ part6/note-redux/src/App.js
@@ -1,8 +1,17 @@
 import NewNote from "./components/NewNote";
 import VisibilityFilter from "./components/VisibilityFilter";
 import Notes from "./components/Notes";
+import { initializeNotes } from "./reducers/noteReducer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initializeNotes());
+  }, [dispatch]);
+
   return (
     <div>
       <NewNote />
--- part6/note-redux/src/index.js
+++ part6/note-redux/src/index.js
@@ -2,21 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
-});
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-console.log(store.getState());
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
